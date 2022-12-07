015c87bcab98d18584328455bd6f03a0df251105
Yesenia Sosa Gonzalez <gonzalez.yesenia0621@gmail.com>
2022-12-07 14:42:38 -0800

    Update

--- models/course.js
+++ models/course.js
@@ -7,6 +7,7 @@ module.exports = (sequelize, DataTypes) => {
     Course.init({
         title: {
             type: DataTypes.STRING,
+            unique: true,
             allowNull: false,
             validate: {
                 notNull: {
