0267a8b4f1935fae3cefe00807ba37b101ce072a
Yesenia Sosa Gonzalez <gonzalez.yesenia0621@gmail.com>
2022-12-06 23:26:31 -0800

    Update

---
+++ models/user.js
@@ -0,0 +1,82 @@
+'use strict';
+
+const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+
+module.exports = (sequelize) => {
+    class User extends Model {}
+    User.init({
+        firstName: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                notNull: {
+                    msg: 'A first name is required'
+                },
+                notEmpty: {
+                    msg: 'Please provide a firt name'
+                }
+            }
+        },
+        lastName: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                notNull: {
+                    msg: 'A last name is required'
+                },
+                notEmpty: {
+                    msg: 'Please provide a last name'
+                }
+            }
+        },
+        emailAddress: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: {
+                msg: 'The email you entered already exists'
+            },
+            validate: {
+                notNull: {
+                    msg: 'An email is required'
+                },
+                isEmail: {
+                    msg: 'Please provide a valid email address'
+                }
+            }
+        },
+        password: {
+            type: DataTypes.VIRTUAL,  
+            allowNull: false,
+            validate: {
+                notNull: {
+                    msg: 'A password is required'
+                },
+                notEmpty: {
+                    msg: 'Please provide a password'
+                },
+                len: {
+                    args: [8, 20],
+                    msg: 'The password should be between 8 and 20 characters in length'
+                }
+            }
+        },
+        confirmedPassword: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            set(val) {
+                if ( val === this.password ) {
+                    const hashedPassword = bcrypt.hashSync(val, 10);
+                    this.setDataValue('confirmedPassword', hashedPassword);
+                }
+            },
+            validate: {
+                notNull: {
+                    msg: 'Both passwords must match'
+                }
+            }
+        }
+    }, { sequelize });
+
+    return User;
+};
