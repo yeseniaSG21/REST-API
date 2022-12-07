2177bae1bd289bf17ee4566c06aea3f54b5afe4e
Yesenia Sosa Gonzalez <gonzalez.yesenia0621@gmail.com>
2022-12-07 14:34:50 -0800

    Update

--- routes/users.js
+++ routes/users.js
@@ -16,3 +16,20 @@ router.get('/users', authenticateUser, asyncHandler(async (req, res) => {
         { firstName, lastName, emailAddress }
     );
 }))
+
+// Route that creates a new user.
+router.post('/', asyncHandler(async (req, res) => {
+    try {
+        await User.create(req.body);
+        res.status(201).json({ "message": "Account successfully created!" });
+    } catch (error) {
+        if (error.name === 'SequelizeValidationError' || error.name === 'SequelizeUniqueConstraintError') {
+            const errors = error.errors.map(err => err.message);
+            res.status(400).json({ errors });   
+        } else {
+            throw error;
+        }
+    }
+}));
+
+module.exports = router;
