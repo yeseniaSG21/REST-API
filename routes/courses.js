6a2ce2f83ef83a57afbcf53ab47d4ec8934810ce
Yesenia Sosa Gonzalez <gonzalez.yesenia0621@gmail.com>
2022-12-07 16:57:24 -0800

    Update

--- routes/courses.js
+++ routes/courses.js
@@ -6,3 +6,6 @@ const { User, Course } = require('../models');
 const { asyncHandler } = require('./middleware/async-handler');
 const { authenticateUser } = require('./middleware/auth-user');
 
+// Construct a router instance.
+const router = express.Router();
+
