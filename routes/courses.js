e101d69cbfdec6082c87ffeaad29651910fd0cbb
Yesenia Sosa Gonzalez <gonzalez.yesenia0621@gmail.com>
2022-12-08 17:31:39 -0800

    Update

--- routes/courses.js
+++ routes/courses.js
@@ -1,7 +1,6 @@
 'use strict';
 
 const express = require('express');
-const bcrypt = require('bcrypt');
 const { User, Course } = require('../models');
 const { asyncHandler } = require('./middleware/async-handler');
 const { authenticateUser } = require('./middleware/auth-user');
